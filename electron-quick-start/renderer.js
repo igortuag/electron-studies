ec9fe49dcd35e5758664ee1150dfc5b6ece46ac8
Igor Tuag <igortuag@gmail.com>
2020-08-25 08:14:44 -0300

    Curso de ElectronJS - IPC (Comunicação entre processos)

--- electron-quick-start/renderer.js
+++ electron-quick-start/renderer.js
@@ -5,8 +5,21 @@
 // selectively enable features needed in the rendering
 // process.
 
-const fs = require('fs');
+const fs = require("fs");
+const { ipcRenderer } = require("electron");
 
 var content = fs.readFileSync(__dirname + "/index.html");
 
 document.querySelector("#teste").innerText = content;
+
+// ipc render - sync
+let result = ipcRenderer.sendSync("sync-ipcmain", "ola ipcrender");
+console.log(result);
+
+// ipc render - async
+
+ipcRenderer.on("async-ipcrender", (event, args) => {
+  console.log("ipcrender", args);
+});
+
+ipcRenderer.send("async-ipcmain", { msg: "oiii" });
