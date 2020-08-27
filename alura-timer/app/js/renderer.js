028169dcf4c50b78b2c3222bd780690b4745681c
Igor Tuag <igortuag@gmail.com>
2020-08-27 12:13:10 -0300

    Iniciando nosso Timer

--- alura-timer/app/js/renderer.js
+++ alura-timer/app/js/renderer.js
@@ -1,7 +1,9 @@
 const { ipcRenderer } = require("electron");
+const timer = require("./timer");
 
 let linkSobre = document.querySelector("#link-sobre");
 let botaoPlay = document.querySelector(".botao-play");
+let tempo = document.querySelector(".tempo");
 
 linkSobre.addEventListener("click", function () {
   ipcRenderer.send("abrir-janela-sobre");
@@ -10,5 +12,6 @@ linkSobre.addEventListener("click", function () {
 let imgs = ["img/play-button.svg", "img/stop-button.svg"];
 botaoPlay.addEventListener("click", function () {
   imgs.reverse();
+  timer.iniciar(tempo);
   botaoPlay.src = imgs[0];
 });
