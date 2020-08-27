f9d68351d013629a0f798c90539e096f0d4dd917
Igor Tuag <igortuag@gmail.com>
2020-08-27 15:10:17 -0300

    Salvando o tempo de estudo

--- alura-timer/data.js
+++ alura-timer/data.js
@@ -5,14 +5,31 @@ module.exports = {
   salvaDados(curso, tempoEstudado) {
     let arquivoDoCurso = __dirname + "/data/" + curso + ".json";
     if (fs.existsSync(arquivoDoCurso)) {
+      this.adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado);
     } else {
-      console.log(arquivoDoCurso)
-      this.criaArquivoDeCurso(arquivoDoCurso, {}).then(() => {});
+      console.log(arquivoDoCurso);
+      this.criaArquivoDeCurso(arquivoDoCurso, {}).then(() => {
+        this.adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado);
+      });
     }
   },
+  adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado) {
+    let dados = {
+      ultimoEstudo: new Date().toString(),
+      tempo: tempoEstudado,
+    };
 
+    jsonfile
+      .writeFile(arquivoDoCurso, dados, {spaces: 2})
+      .then(() => {
+        console.log("Tempo salvo com sucesso");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
   criaArquivoDeCurso(nomeArquivo, conteudoArquivo) {
-    console.log(nomeArquivo)
+    console.log(nomeArquivo);
     return jsonfile
       .writeFile(nomeArquivo, conteudoArquivo)
       .then(() => {
