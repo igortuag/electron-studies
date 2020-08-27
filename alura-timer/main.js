fb6254bd57a8ccc9fd08232c8c3c091a15cbba1c
Igor Tuag <igortuag@gmail.com>
2020-08-27 08:42:48 -0300

    Criando a primeira janela

--- alura-timer/main.js
+++ alura-timer/main.js
@@ -1,1 +1,11 @@
-console.log("Olá Mundo!")
+const { app, BrowserWindow } = require("electron");
+
+app.on("ready", () => {
+  console.log("Aplication starts");
+  let mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+  });
+
+  mainWindow.loadURL('http://www.google.com.br/')
+});
