5f5b1f73e7ca9352a11fee8c18d4f988b1866308
Igor Tuag <igortuag@gmail.com>
2020-08-27 08:58:25 -0300

    As tecnologias Web e os processos

--- alura-timer/main.js
+++ alura-timer/main.js
@@ -1,11 +1,15 @@
 const { app, BrowserWindow } = require("electron");
 
 app.on("ready", () => {
-  console.log("Aplication starts");
+  console.log("Application starts");
   let mainWindow = new BrowserWindow({
     width: 600,
     height: 400,
   });
 
-  mainWindow.loadURL('http://www.google.com.br/')
+  mainWindow.loadURL(`${__dirname}/app/index.html`);
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
 });
