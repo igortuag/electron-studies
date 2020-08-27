75ea291b9ff83ddb67aa3dc6a169b4e229dbbe8d
Igor Tuag <igortuag@gmail.com>
2020-08-27 13:54:22 -0300

    Enviando dados para outro processo

--- alura-timer/app/js/timer.js
+++ alura-timer/app/js/timer.js
@@ -1,19 +1,25 @@
 const moment = require("moment");
+const { ipcRenderer } = require("electron");
 let segundos;
-let timer
+let timer;
+let tempo;
 
 module.exports = {
   iniciar(el) {
-    let tempo = moment.duration(el.textContent);
+    tempo = moment.duration(el.textContent);
     segundos = tempo.asSeconds();
     clearInterval(timer);
+    
     timer = setInterval(() => {
       segundos++;
       el.textContent = this.segundosParaTempo(segundos);
     }, 1000);
   },
-  parar() {
+  parar(curso) {
     clearInterval(timer);
+    let tempoEstudado = this.segundosParaTempo(segundos);
+
+    ipcRenderer.send("curso-parado", curso, tempoEstudado);
   },
   segundosParaTempo(segundos) {
     return moment().startOf("day").seconds(segundos).format("HH:mm:ss");
--- alura-timer/main.js
+++ alura-timer/main.js
@@ -41,3 +41,7 @@ ipcMain.on("abrir-janela-sobre", () => {
 ipcMain.on("fechar-janela-sobre", () => {
   sobreWindow.close();
 });
+
+ipcMain.on('curso-parado', (event, curso, tempoEstudado) => {
+  console.log(`O curso ${curso} foi estudado por ${tempoEstudado}`)
+})
