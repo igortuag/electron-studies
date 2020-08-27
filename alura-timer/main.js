4bead62e64e862bc7abfebb80323fed12e542812
Igor Tuag <igortuag@gmail.com>
2020-08-27 09:55:46 -0300

    Criando a janela sobre

--- alura-timer/main.js
+++ alura-timer/main.js
@@ -1,10 +1,13 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 
 app.on("ready", () => {
   console.log("Application starts");
   let mainWindow = new BrowserWindow({
     width: 600,
     height: 400,
+    webPreferences: {
+      nodeIntegration: true,
+    }
   });
 
   mainWindow.loadURL(`${__dirname}/app/index.html`);
@@ -13,3 +16,12 @@ app.on("ready", () => {
 app.on("window-all-closed", () => {
   app.quit();
 });
+
+ipcMain.on('abrir-janela-sobre', () => {
+  let sobreWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+  });
+
+  sobreWindow.loadURL(`file://${__dirname}/app/sobre.html`)
+})
