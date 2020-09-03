60993da771a4b8e3e2c532e8e60b175fa1f6142d
Igor Tuag <igortuag@gmail.com>
2020-09-03 07:37:56 -0300

    Enviando informações para o front end

--- prap/index.js
+++ prap/index.js
@@ -1,17 +1,18 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const sizeOf = require("image-size");
-
+let mainWindow
 app.on("ready", () => {
-  const janelaPrincipal = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
     },
   });
-  janelaPrincipal.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on("obterDimensoesDaImagem", (event, path) => {
   sizeOf(path, function (err, dimensions) {
-    console.log("largura:" + dimensions.width, "altura:" + dimensions.height);
+    mainWindow.webContents.send('dimensoesDaImagem', dimensions)
+    console.log("largura:" + dimensions.width, "altura:" + dimensions.height );
   });
 });
