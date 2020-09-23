02168c95772809516b2267828f7f1bd42a0920a2
Igor Tuag <igortuag@gmail.com>
2020-09-23 09:29:59 -0300

    Vendo o tempo do cronometro

--- terap/index.js
+++ terap/index.js
@@ -1,8 +1,9 @@
 const electron = require("electron");
 const ChronoTray = require("./app/chronotray");
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
+let tray;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({
@@ -14,9 +15,17 @@ app.on("ready", () => {
     skipTaskbar: true,
   });
 
-  const tray = new ChronoTray(`${__dirname}/robot.png`, mainWindow);
+  tray = new ChronoTray(`${__dirname}/robot.png`, mainWindow);
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   mainWindow.on("blur", () => {
     setTimeout(() => mainWindow.hide());
   });
+});
+
+ipcMain.on("timeUpdate", (event, timeUpdate) => {
+  if (process.platform === "darwin") {
+    tray.setTitle(timeUpdate);
+  } else {
+    tray.setToolTip(timeUpdate);
+  }
 });
